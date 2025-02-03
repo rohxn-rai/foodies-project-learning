6267499a3ffad3ce673bf2e6b579813b2b68b1c4
rohxn-rai <rohan.rai932@outlook.com>
2025-02-03 04:37:49 -0500

    added Loading indicator - used Suspense fallback to show loading of data.

--- app/meals/loading-out.js
+++
@@ -1,7 +0,0 @@
-import classes from "./loading.module.css";
-
-const MealsLoadingPage = () => {
-  return <p className={classes.loading}>Fetching meals...</p>;
-};
-
-export default MealsLoadingPage;
--- app/meals/page.js
+++ app/meals/page.js
@@ -3,10 +3,14 @@ import Link from "next/link";
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
 import getMeals from "@/data/meals";
+import { Suspense } from "react";
+
+async function Meals() {
+  const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
 
 const MealsPage = async () => {
-  const meals = await getMeals();
-
   return (
     <>
       <header className={classes.header}>
@@ -22,7 +26,11 @@ const MealsPage = async () => {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching meals...</p>}
+        >
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
