8b4613aa4c8f9969ecb6ce2aa51b785cb9b06f66
rohxn-rai <rohan.rai932@outlook.com>
2025-02-03 03:27:43 -0500

    created Meals page

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,5 +1,24 @@
+import Link from "next/link";
+import classes from "./page.module.css";
+
 const meals = () => {
-  return <h1>Meals Page</h1>;
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created
+          <span className={classes.highlight}> by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your favorite recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}></main>
+    </>
+  );
 };
 
 export default meals;
