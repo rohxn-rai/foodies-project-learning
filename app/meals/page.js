0a1ff8e46689f8d1d852582c34f3524374cd46c3
rohxn-rai <rohan.rai932@outlook.com>
2025-02-03 05:41:41 -0500

    organized the code

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,9 +1,9 @@
+import { Suspense } from "react";
 import Link from "next/link";
 
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/data/meals";
-import { Suspense } from "react";
 
 async function Meals() {
   const meals = await getMeals();
