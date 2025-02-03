b6ad653379c3eae235f006771800a23fb5be8d77
rohxn-rai <rohan.rai932@outlook.com>
2025-02-03 08:15:53 -0500

    modified button - this component checks that status of the form submission and then returns submitting on pending state

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -4,6 +4,7 @@ import { shareMeal } from "@/data/action";
 
 import ImagePicker from "@/components/meals/image-picker";
 import classes from "./page.module.css";
+import MealsFormSubmit from "@/components/meals/meals-form-submit";
 
 const ShareMealPage = () => {
   return (
@@ -45,7 +46,7 @@ const ShareMealPage = () => {
           </p>
           <ImagePicker label="Your image" name="image" />
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsFormSubmit />
           </p>
         </form>
       </main>
