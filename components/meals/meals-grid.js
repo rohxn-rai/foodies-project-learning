615ea0193f610403b3d994ba7904c2f3ad84779f
rohxn-rai <rohan.rai932@outlook.com>
2025-02-03 03:46:11 -0500

    connected the MealItem component into the MealsGrid component

--- components/meals/meals-grid.js
+++ components/meals/meals-grid.js
@@ -1,10 +1,13 @@
+import MealItem from "./meal-item";
 import classes from "./meals-grid.module.css";
 
 const MealsGrid = ({ meals }) => {
   return (
-    <ul>
+    <ul className={classes.meals}>
       {meals.map((meal) => (
-        <li key={meal.id}></li>
+        <li key={meal.id}>
+          <MealItem {...meal} />
+        </li>
       ))}
     </ul>
   );
