6e522eb0418142c6b60ce736e48ade6e0997122a
rohxn-rai <rohan.rai932@outlook.com>
2025-02-03 05:41:45 -0500

    added not found page - for [meal details] page

--- app/meals/[mealDetails]/page.js
+++ app/meals/[mealDetails]/page.js
@@ -1,9 +1,15 @@
+import Image from "next/image";
+import { notFound } from "next/navigation";
+
 import { getMeal } from "@/data/meals";
 import classes from "./page.module.css";
-import Image from "next/image";
 
 const mealDetailsPage = ({ params }) => {
   const meal = getMeal(params.mealDetails);
+
+  if (!meal) {
+    notFound();
+  }
 
   meal.instructions = meal.instructions.replace(/\n/g, "<br />");
 
